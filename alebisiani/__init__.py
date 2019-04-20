e961c935dc6e7cdd97776ea20317ddb6f4e9e0d2
alessandrobisiani <bisi90@gmail.com>
2019-04-20 12:31:18 -0400

    feat: Set up pipenv and basic app factory, tested

---
+++ alebisiani/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/src/env python3
+import os
+from flask import Flask
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'alebisiani.sqlite'),
+    )
+
+    # Set config depending on whether Flask instance is for testing
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    return app
